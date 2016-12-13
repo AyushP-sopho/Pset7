9226f4aa815585ca5b98dcee25a1d8e44f832322
Ayush Prakash <cs15btech11008@iith.ac.in>
2016-12-13 07:26:59 +0000

    Got the permissions right

---
+++ views/get_symbol.php
@@ -0,0 +1,13 @@
+<form action="quote.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autocomplete="off" autofocus class ="form-control" name="symbol" placeholder="Symbol" type="text"/>
+        </div>
+        <div>
+            <button class="btn btn-default" type="submit">
+                <span aria-hidden="true" class="glyphicon glyphicon-arrow-right"></span>
+                Get Quote
+            </button> 
+        </div>
+    </fieldset>
+</form>
