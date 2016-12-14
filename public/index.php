e5eea8e9a2e8d18b45f3628ddbfd424e869d0cd4
Ayush Prakash <cs15btech11008@iith.ac.in>
2016-12-14 08:21:52 +0000

    Building Your Portfolio

--- public/index.php
+++ public/index.php
@@ -3,7 +3,23 @@
     // configuration
     require("../includes/config.php"); 
 
-    // render portfolio
-    render("portfolio.php", ["title" => "Portfolio"]);
-
+    $id=CS50::query("SELECT id FROM users WHERE username=?",$_POST["username"]);
+    $rows=CS50::query("SELECT * FROM shares WHERE user_id=?",$id);
+    
+    $position=[];
+    foreach($rows as $row)
+    {
+        $stock=lookup($row["symbol"]);
+        if($stock!=false)
+        {
+            $position[]=[
+                "name" => $stock["name"],
+                "symbol" => $row["symbol"],
+                "price" => $stock["price"],
+                "shares" => $row["shares"]
+            ];
+        }
+    }
+    render("portfolio.php",["title" => "Portfolio","position" => $position]);
+    
 ?>
